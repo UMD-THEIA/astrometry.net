d33777cf86ef94505fcdc803cb7e8b878df4393f
mBuschauer <marco@buschauer.us>
2024-10-28 20:23:43 -0400

    changed to shell

---
+++ shell.nix
@@ -0,0 +1,37 @@
+{ pkgs ? import <nixpkgs> { } }:
+pkgs.mkShell {
+  nativeBuildInputs = with pkgs.buildPackages; [
+    libgcc
+    gnumake
+    file
+
+
+    cairo
+    netpbm
+    libpng
+    libjpeg
+    libz
+    bzip2
+    zlib
+
+
+    python311
+    python311Packages.numpy
+    python311Packages.astropy
+    python311Packages.scipy
+    python311Packages.setuptools
+    python311Packages.pillow
+    python311Packages.tkinter
+    python311Packages.wheel
+    python311Packages.matplotlib
+
+
+    swig4
+    cfitsio
+    gsl
+
+    pkg-config
+    wcslib
+  ];
+
+}
